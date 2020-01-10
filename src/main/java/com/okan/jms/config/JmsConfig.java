7afa0d7c1d83e0a095d0326f73407b4e9107d141
Okan Hollander <o.hollander1990@gmail.com>
2020-01-10 21:08:21 +0100

    Message Converter Configuration

---
+++ src/main/java/com/okan/jms/config/JmsConfig.java
@@ -0,0 +1,25 @@
+package com.okan.jms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     10/01/2020
+ * Time:     21:04
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
